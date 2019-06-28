a37a153bb7222f98f3e375b0e0319ef78604775f
Rich Lane <lanerl@gmail.com>
2019-06-28 10:03:39 -0700

    install mods in dev folder

--- install_mods.sh
+++ install_mods.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 test -f rlane-oni-mods.sln || exit 1  # Must be run from root of the repository.
-DST=~/Documents/Klei/OxygenNotIncluded/mods/local
+DST=~/Documents/Klei/OxygenNotIncluded/mods/dev
 for MOD in HeatingElement Alarm MeteorDefenseLaser Endpoint; do
 	mkdir -p $DST/$MOD
 	cp $MOD/bin/Debug/$MOD.dll $DST/$MOD/
