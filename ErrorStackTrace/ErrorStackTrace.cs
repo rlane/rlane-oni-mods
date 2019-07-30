563a8e0bbf89ed850fd94b850221bfed33df4877
Rich Lane <lanerl@gmail.com>
2019-07-30 12:51:56 -0700

    ErrorStackTrace: initial commit

---
+++ ErrorStackTrace/ErrorStackTrace.cs
@@ -0,0 +1,13 @@
+﻿using Harmony;
+
+namespace ErrorStackTrace
+{
+    [HarmonyPatch(typeof(KCrashReporter), "ShowDialog")]
+    internal class ErrorStackTrace_KCrashReporter_ShowDialog
+    {
+        private static void Prefix(string stack_trace)
+        {
+            Debug.Log("Stack trace:\n" + stack_trace);
+        }
+    }
+}
--- install_mods.sh
+++ install_mods.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 test -f rlane-oni-mods.sln || exit 1  # Must be run from root of the repository.
 DST=~/Documents/Klei/OxygenNotIncluded/mods/dev
-for MOD in HeatingElement Alarm MeteorDefenseLaser Endpoint; do
+for MOD in HeatingElement Alarm MeteorDefenseLaser Endpoint ErrorStackTrace; do
 	mkdir -p $DST/$MOD
 	cp $MOD/bin/Debug/$MOD.dll $DST/$MOD/
 done
