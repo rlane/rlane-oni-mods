9901c8c04db284c1659d6b33b1cee302d2c8f05f
Rich Lane <lanerl@gmail.com>
2019-08-27 18:11:37 -0700

    Stimulant: initial commit

---
+++ Stimulant/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Stimulant")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Stimulant")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("be805285-cab4-441e-b0b3-7547863231e8")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Stimulant/StimulantConfig.cs
@@ -0,0 +1,74 @@
+﻿// TODO: Remove "Immune Booster" from the tooltip.
+using System.Collections.Generic;
+using Klei.AI;
+using UnityEngine;
+
+namespace Stimulant
+{
+    public class StimulantConfig : IEntityConfig
+    {
+        public const string ID = "Stimulant";
+
+        public static ComplexRecipe recipe;
+
+        public static readonly MedicineInfo medicineInfo = new MedicineInfo("Stimulant", "Medicine_Stimulant", MedicineInfo.MedicineType.Booster);
+
+        public GameObject CreatePrefab()
+        {
+            Db.Get().effects.Add(CreateEffect());
+
+            GameObject gameObject = EntityTemplates.CreateLooseEntity("Stimulant", "Stimulant", "Temporarily increases physical ability at the cost of increased metabolism.", 1f, unitMass: true, Assets.GetAnim("pill_2_kanim"), "object", Grid.SceneLayer.Front, EntityTemplates.CollisionShape.RECTANGLE, 0.8f, 0.4f, isPickupable: true);
+            EntityTemplates.ExtendEntityToMedicine(gameObject, medicineInfo);
+            ComplexRecipe.RecipeElement[] inputs = new ComplexRecipe.RecipeElement[3]
+            {
+                new ComplexRecipe.RecipeElement(SimHashes.RefinedCarbon.CreateTag(), 10f),
+                new ComplexRecipe.RecipeElement(SpiceNutConfig.ID, 1f),
+                new ComplexRecipe.RecipeElement("LightBugEgg", 1f)
+            };
+            ComplexRecipe.RecipeElement[] outputs = new ComplexRecipe.RecipeElement[1]
+            {
+                new ComplexRecipe.RecipeElement("Stimulant", 10f)
+            };
+            string id = ComplexRecipeManager.MakeRecipeID("Apothecary", inputs, outputs);
+            recipe = new ComplexRecipe(id, inputs, outputs)
+            {
+                time = 75f,
+                description = "Temporarily increases physical ability at the cost of increased metabolism.",
+                nameDisplay = ComplexRecipe.RecipeNameDisplay.Result,
+                fabricators = new List<Tag> { "Apothecary" },
+                sortOrder = 1
+            };
+            return gameObject;
+        }
+
+        private static Effect CreateEffect()
+        {
+            var effect = new Effect("Medicine_Stimulant", "Stimulant", "Temporarily increases physical ability at the cost of increased metabolism.", 600f, show_in_ui: true, trigger_floating_text: true, is_bad: false);
+            var attributes = new List<string> {
+                Db.Get().Attributes.Athletics.Id,
+                Db.Get().Attributes.Strength.Id,
+                Db.Get().Attributes.Digging.Id,
+                Db.Get().Attributes.Construction.Id,
+            };
+            foreach (var attribute in attributes)
+            {
+                effect.Add(new AttributeModifier(attribute, 2, effect.Name));
+                effect.Add(new AttributeModifier(attribute, 0.5f, effect.Name, is_multiplier: true));
+            }
+            var calorieIncrease = 500 * (5.0f/3);
+            effect.Add(new AttributeModifier(Db.Get().Amounts.Calories.deltaAttribute.Id, -calorieIncrease, effect.Name));
+            //effect.Add(new AttributeModifier("CaloriesMax", calorieIncrease * 1000, effect.Name));
+            effect.Add(new AttributeModifier("AirConsumptionRate", 0.05f, effect.Name));
+            return effect;
+        }
+
+        public void OnPrefabInit(GameObject inst)
+        {
+        }
+
+        public void OnSpawn(GameObject inst)
+        {
+        }
+    }
+
+}
