5b186a161efb2d962b8b28a69c425daaa2d07302
Rich Lane <lanerl@gmail.com>
2020-03-28 14:06:43 -0700

    Ruins backend: use a separate key for signing policy documents

--- Ruins/backend/main.py
+++ Ruins/backend/main.py
@@ -3,19 +3,25 @@
 # requests.post(r['url'], data=r['form'], files={'file': 'abcdefg'})
 
 import random
+import os
 from flask import Flask
 from flask import jsonify
 from google.cloud import storage
 
 
+APPID = 'oni-ruins-test'
+ACCOUNT_KEY_PATH = '/tmp/service-account.json'
 app = Flask(__name__)
-storage_client = storage.Client()
+
+
+storage.Client().bucket(APPID + '.appspot.com').blob('secrets/service-account.json').download_to_filename(ACCOUNT_KEY_PATH)
+storage_client = storage.Client.from_service_account_json(ACCOUNT_KEY_PATH)
+os.unlink(ACCOUNT_KEY_PATH)
 
 
 @app.route('/generate_upload_url')
 def generate_upload_url():
-    appid = 'oni-ruins-test'
-    bucket = storage_client.get_bucket(appid + '.appspot.com')
+    bucket = storage_client.get_bucket(APPID + '.appspot.com')
     filename = 'ruin.%d.yaml' % random.randint(0, 1<<64)
     policy = bucket.generate_upload_policy(
             conditions=[
@@ -26,7 +32,7 @@ def generate_upload_url():
         'url': 'https://storage.googleapis.com/' + bucket.name,
         'form': {
            'key': filename,
-           'GoogleAccessId': '%s@%s.iam.gserviceaccount.com' % (appid, appid),
+           'GoogleAccessId': '%s@%s.iam.gserviceaccount.com' % (APPID, APPID),
            'policy': policy['policy'],
            'signature': policy['signature'],
            'Content-Type': 'text/yaml'}})
