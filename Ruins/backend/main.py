d7731e43a0286876218ba9bc421ac74b2190238b
Rich Lane <lanerl@gmail.com>
2020-03-28 17:00:44 -0700

    Ruins backend: add expiration time

--- Ruins/backend/main.py
+++ Ruins/backend/main.py
@@ -4,6 +4,8 @@
 
 import random
 import os
+import datetime
+import logging
 from flask import Flask
 from flask import jsonify
 from google.cloud import storage
@@ -23,7 +25,10 @@ os.unlink(ACCOUNT_KEY_PATH)
 def generate_upload_url():
     bucket = storage_client.get_bucket(APPID + '.appspot.com')
     filename = 'ruins/%d.yaml' % random.randint(0, 1<<64)
+    expiration = datetime.datetime.utcnow() + datetime.timedelta(seconds=10)
+    logging.info('Generating upload URL for blob %s', filename)
     policy = bucket.generate_upload_policy(
+            expiration=expiration,
             conditions=[
                 ['eq', '$key', filename],
                 ['content-length-range', 0, 1000000],
